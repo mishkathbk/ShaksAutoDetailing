f8d2537530a8d8baa283692382325cdee19f1d27
mishkathbk <mishkathbk@gmail.com>
2024-08-05 01:19:12 +0530

    pushh

---
+++ src/app/_components/SectionOne.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import React from "react";
+import BgImage from "../../../public/images/carBg.jpg";
+import { ButtonAnimation } from "@/components/ButtonAnimation";
+const SectionOne = () => {
+  return (
+    <div className="relative flex justify-center items-center h-[85dvh] w-full">
+      <Image
+        src={BgImage}
+        alt=""
+        className=" h-full w-full object-cover"
+        fill
+      />
+      <div className="flex flex-col font-poppins justify-center items-center z-10  text-gray-50">
+        <div className="text-[#f72] ~text-title-medium/label-footer font-bold">
+          Car Washing & Detailing
+        </div>
+        <div className="~text-headline-large/display-medium text-center py-[1rem] font-bold">Keep Your Car Newer</div>
+        <div className="~text-body-medium/small-screen ">
+          We provide top-notch car detailing services
+        </div>
+        <ButtonAnimation className=" rounded-2xl bg-darkOrange mt-[1.5rem] hover:bg-white">
+          <div className="py-[0.5rem] px-[1rem] text-body-medium text-white font-bold hover:text-darkOrange">
+            Book Now
+          </div>
+        </ButtonAnimation>
+      </div>
+    </div>
+  );
+};
+
+export default SectionOne;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+
+import EnvelopeSolid from "../../../public/svg/email.svg";
+import LocationDotSolid from "../../../public/svg/address.svg";
+import PhoneSolid from "../../../public/svg/phone.svg";
+import BgImage from "../../../public/images/wash4.jpg";
+
+const page = () => {
+  return (
+    <div className="relative z-[10rem] flex h-[90vh] items-center justify-center">
+      <Image src={BgImage} alt="Bg-Image" fill className="object-cover" />
+      {/* <div className="from-1% to-99% absolute inset-0  z-[1] bg-gradient-to-t from-[#000000] to-transparent opacity-50"></div> */}
+      <div className="relative flex flex-col items-center justify-center gap-[2.25rem] px-[1rem] text-left ">
+        <h2 className="~text-case-study-title/display-hero text-darkOrange font-poppins font-bold ">
+          Contact Us
+        </h2>
+        <div className="flex flex-col  md:flex-row md:items-center justify-center ~gap-[1.5rem]/[2rem] border-2 bg-white ~p-[0.5rem]/[2.5rem] rounded-xl">
+          <div className="flex items-center md:justify-center gap-[1rem]">
+            <div className="flex  items-center justify-center rounded-full bg-white">
+              <EnvelopeSolid className="size-[1.5rem]" />
+            </div>
+            <div className="flex flex-col items-center justify-center w-full">
+              <h2 className="text-title-medium font-poppins text-darkBlue font-bold">
+                ADDRESS
+              </h2>
+              <h2 className="text-title-small font-poppins">Halifax, Canada</h2>
+            </div>
+          </div>
+          <div className="flex items-center md:justify-center gap-[1rem]">
+            <div className="flex  items-center justify-center rounded-full bg-white">
+              <PhoneSolid className="size-[1.5rem]" />
+            </div>
+            <div className="flex flex-col items-center justify-center w-full ">
+              <h2 className="text-title-medium font-poppins  text-darkBlue font-bold">
+                CALL
+              </h2>
+              <h2 className="text-title-small font-poppins"> +1 909 900 9439</h2>
+            </div>
+          </div>
+          <div className="flex items-center md:justify-center gap-[1rem]">
+            <div className="flex  items-center justify-center rounded-full bg-white">
+              <LocationDotSolid className="size-[1.5rem]" />
+            </div>
+            <div className="flex flex-col items-center justify-center w-full">
+              <h2 className="text-title-medium font-poppins text-darkBlue font-bold">
+                EMAIL
+              </h2>
+              <h2 className="text-title-small font-poppins">
+                Info@shaksautodetailinghalifax.ca
+              </h2>
+            </div>
+          </div>
+        </div>
+        {/* <button className=" hover:bg-blue-900 w-fit rounded-xl bg-blue-600 px-[2.5rem] py-[0.9rem] text-global-font-tab-text2 font-bold text-white">
+                    CONTACT US
+                </button> */}
+      </div>
+    </div>
+  );
+};
+
+export default page;
